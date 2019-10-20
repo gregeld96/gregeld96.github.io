3bdaf64884e3d790e6e74d4dd7c6c43bc5fe8e54
Gregorius Eldwin Pradipta <gregeld96@gmail.com>
2019-10-20 18:05:26 +0800

    Initial

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $("button").on('click', function() {
+   if ($(this).text() =='Read Less'){
+   	$(this).text('Read More');
+   }
+   else {
+   	$(this).text('Read Less');
+    }
+   });
+  });
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body, .page-scroll').animate({
+        scrollTop: $(hash).offset().top
+      }, 1200, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
